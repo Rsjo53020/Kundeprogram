943add67214c360e8c1cec18eaa155c677fdc965
Alexander DESKTOP-85QQ2VK <alexander.c.larsen@gmail.com>
2023-10-04 11:16:20 +0200

    BLL Services ARV

--- BLL/Services/BusinessCustomerService.cs
+++ BLL/Services/BusinessCustomerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ABS.Interfaces.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,29 @@ namespace BLL.Services
 {
     public class BusinessCustomerService : BaseCustomerService
     {
+        public override void AddCustomer(ICustomerModel customer)
+        {
+            throw new NotImplementedException();
+        }
 
+        public override void DeleteCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GetCustomerById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateCustomer(ICustomerModel customer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
