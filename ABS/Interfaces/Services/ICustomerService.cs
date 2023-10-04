578669449a46cd7642ccac96971bf139f8258c03
Alexander DESKTOP-85QQ2VK <alexander.c.larsen@gmail.com>
2023-10-04 10:33:40 +0200

    ICustomerService Push

--- ABS/Interfaces/Services/ICustomerService.cs
+++ ABS/Interfaces/Services/ICustomerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ABS.Interfaces.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,12 @@ using System.Threading.Tasks;
 namespace ABS.Interfaces.Services
 {
     public interface ICustomerService
-{
-}
+    {
+        public int Id { get; set; }
+        public string FirtName { get; set; }
+        public string LastName { get; set; }
+        public string Phonenumber { get; set; }
+        public string Email { get; set; }
+        public IAdressModel IAdressModel { get; set; }
+    }
 }
