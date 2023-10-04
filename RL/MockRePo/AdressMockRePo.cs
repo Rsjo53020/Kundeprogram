f48dd439ba20e2d95e755e2a6c24e5b577d97130
CHRISTOFFER\chris <chsk52994@edu.ucl.dk>
2023-10-04 10:52:39 +0200

    flyt data

--- RL/MockRePo/AdressMockRePo.cs
+++ RL/MockRePo/AdressMockRePo.cs
@@ -8,56 +8,7 @@ namespace RePo.MockRePo
 {
     public class AdressMockRePo
     {
-        List<ABS.Interfaces.Models.ICustomerModel> customers = 
-            new List<ABS.Interfaces.Models.ICustomerModel> ();
-
-        /// <summary>
-        /// (C)RUD
-        /// </summary>
-        /// <param name="customer"></param>
-        public void CreateCustomer(ABS.Interfaces.Models.ICustomerModel customer)
-        {
-
-        }
-
-        /// <summary>
-        /// C(R)UD
-        /// </summary>
-        /// <param name="ID"></param>
-        /// <returns></returns>
-        public ABS.Interfaces.Models.ICustomerModel GetCustomer(int ID)
-        {
-            return null;
-        }
-        /// <summary>
-        /// C(R)UD
-        /// </summary>
-        /// <param name="ID"></param>
-        /// <returns></returns>
-        public List<ABS.Interfaces.Models.ICustomerModel> GetAllCustomers()
-        {
-            return customers;
-        }
-
-        /// <summary>
-        /// CR(U)D
-        /// </summary>
-        /// <param name="ID"></param>
-        /// <returns></returns>
-        public void UpdateCustomer(int ID)
-        {
-           
-        }
-
-        /// <summary>
-        /// CRU(D)
-        /// </summary>
-        /// <param name="ID"></param>
-        /// <returns></returns>
-        public bool DeleteCustomer(int ID)
-        {
-            return true;
-        }
+        
 
 
 
