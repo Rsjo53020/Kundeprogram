193b3358ce2348b58cf3c30e895c94330186c42a
RS\ronni <rsjo53020@edu.ucl.dk>
2023-10-11 09:52:04 +0200

    new

---
+++ RL/DBRePo/CustomerDBRepoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RePo.DBRePo
+{
+    internal class CustomerDBRepoContext : DdContext
+    {
+        //optionsBuilder.UseSqlServer("");
+
+        //add-migration sigendeting
+
+        //update-databasen
+    }
+}
