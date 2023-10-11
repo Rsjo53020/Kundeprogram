fed18876b47ac5fe7f04a17f6a26786a8a363719
Alexander DESKTOP-85QQ2VK <alexander.c.larsen@gmail.com>
2023-10-11 10:14:40 +0200

    UnitTestAdd

--- UnitTest/UnitTestCustomer.cs
+++ UnitTest/UnitTestCustomer.cs
@@ -4,7 +4,50 @@ namespace UnitTest
     public class UnitTestCustomer
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddCustomer()
+        {
+            //Arrange
+            ABS.Interfaces.Services.ICustomerService customerService = new BLL.Services.CustomerService();
+            
+            //Act
+            customerService.AddCustomer("Test", "Test", "Test", "Test");
+
+            //Assert
+            return true;
+        }
+
+        [TestMethod]
+        public void DeleteCustomer()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void GetAllCustomers()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void GetCustomerById()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void UpdateCustomer()
         {
             //Arrange
 
