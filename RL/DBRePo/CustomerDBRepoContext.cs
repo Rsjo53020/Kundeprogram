fe14098fe56c4e4683a75cac501b16bbf597e80a
CHRISTOFFER\chris <chsk52994@edu.ucl.dk>
2023-10-15 19:10:06 +0200

    SqlRePo

--- RL/DBRePo/CustomerDBRepoContext.cs
+++ RL/DBRePo/CustomerDBRepoContext.cs
@@ -10,13 +10,15 @@ namespace RePo.DBRePo
         public DbSet<ModelsRePo.Customer> Customers { get; set; } = null!;
         public DbSet<ModelsRePo.Adress> Adresses { get; set; } = null!;
 
+        //man må ikke lave en constructor 
+
         //public CustomerDBRepoContext()
         //{
         //    connPath = ConfigurationManager.ConnectionStrings["default"].ToString();
         //}
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
+        { // kan ikke finde ud af at gemme strengen væk
             optionsBuilder.UseSqlServer("Data Source=mssql12.unoeuro.com;Initial Catalog=aleeex_dk_db_database;Persist Security Info=True;User ID=aleeex_dk;Password=Ghtbd2R6wkxa3Efmycg9;TrustServerCertificate=True;");
         }
 
---
+++ RL/DBRePo/SqlRePo.cs
@@ -0,0 +1,46 @@
+﻿using ABS.Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RePo.DBRePo
+{
+    public class SqlRePo : ABS.Interfaces.RePo.ICustomerRePo
+    {
+        public bool CreateCustomer(string firstname, string lastname, string phonenumber, string email)
+        {
+            using(CustomerDBRepoContext context = new CustomerDBRepoContext())
+            {
+                return context;
+            }
+        }
+
+        public bool DeleteCustomer(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ICustomerModel> GetAllCustomers()
+        {
+            using(CustomerDBRepoContext context = new CustomerDBRepoContext()) 
+            {
+                return context.Customers.ToList<ICustomerModel>();
+            }
+        }
+
+        public ICustomerModel GetCustomerById(int ID)
+        {
+            using(CustomerDBRepoContext context = new CustomerDBRepoContext())
+            {
+                return context.Customers.Where(x => x.Id == ID).FirstOrDefault();
+            }
+        }
+
+        public bool UpdateCustomer(int id, string firstname, string lastname, string phonenumber, string mail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
