03cce5a32ffd8676da68f71666c9dc97cb9f2096
RS\ronni <rsjo53020@edu.ucl.dk>
2023-10-15 23:33:26 +0200

    now with validate

--- AllInOne/Form1.cs
+++ AllInOne/Form1.cs
@@ -3,6 +3,7 @@ using ABS.Interfaces.Services;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using RePo.ModelsRePo;
 using System.ComponentModel;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace AllInOne
@@ -11,6 +12,44 @@ namespace AllInOne
     {
         private readonly ABS.Interfaces.Services.ICustomerService _customerService;
         List<ABS.Interfaces.Models.ICustomerModel> _customers;
+
+        /// <summary>
+        /// helpMetode to validate phone number format
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <returns></returns>
+        private bool IsValidPhoneNumber(string phoneNumber)
+        {
+            // kontrollere om telefonnummeret følger det ønskede mønster
+            string pattern = @"^\d{1,8}$"; // Eksempel: 10203040
+            return Regex.IsMatch(phoneNumber, pattern); //A Regular Expression (or Regex)
+
+
+        }
+
+        /// <summary>
+        /// helpMetode to validate the e-mail format
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        private bool IsValidEmail(string email)
+        {
+            // kontroller om e-mailadressen følger det ønskede mønster
+            string pattern = @"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            //var resultat = pattern.Contains("@");
+
+
+            if (pattern.Contains('@'))
+            {
+                return Regex.IsMatch(email, pattern);
+            }
+            else
+            {
+                MessageBox.Show("Ugyldigt e-mail - mangler et @.", "Fejl", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
         public Form1(ABS.Interfaces.Services.ICustomerService customerService)
         {
             InitializeComponent();
@@ -23,11 +62,50 @@ namespace AllInOne
             dataGridView1.DataSource = customerModels;
         }
 
+        /// <summary>
+        /// Click event there add a customer. the metode validate the imput from UI
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BTN_AddCustomer_Click(object sender, EventArgs e)
         {
-            _customerService.AddCustomer(TB_FirstName.Text, TB_LastName.Text, TB_PhoneNr.Text, TB_Mail.Text);
+            // Valider input
+            if (string.IsNullOrEmpty(TB_FirstName.Text) || string.IsNullOrEmpty(TB_LastName.Text) || string.IsNullOrEmpty(TB_PhoneNr.Text) || string.IsNullOrEmpty(TB_Mail.Text))
+            {
+                MessageBox.Show("Alle felter skal udfyldes.", "Fejl", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Valider telefonnummer format
+            if (!IsValidPhoneNumber(TB_PhoneNr.Text))
+            {
+                MessageBox.Show("Ugyldigt telefonnummer format.", "Fejl", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Valider e-mail format
+            if (!IsValidEmail(TB_Mail.Text))
+            {
+                MessageBox.Show("Ugyldigt e-mail format.", "Fejl", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Fortsæt med at tilføje kunden
+            bool success = _customerService.AddCustomer(TB_FirstName.Text, TB_LastName.Text, TB_PhoneNr.Text, TB_Mail.Text);
+            // Tjek om oprettelsen var vellykket
+            if (success)
+            {
+                MessageBox.Show("Kunden blev oprettet succesfuldt.", "Gennemført", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+            }
+            else
+            {
+                MessageBox.Show("Der opstod en fejl under oprettelsen af kunden.", "Gennemført", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+            }
             Form1_Load(sender, e);
         }
+
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
@@ -74,15 +152,37 @@ namespace AllInOne
             TB_Mail.Text = customer.Email;
 
         }
-
+        /// <summary>
+        /// find a customer by ID and validate the input
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BTN_ShowOneCustomer_Click(object sender, EventArgs e)
         {
-            var Customer = _customerService.GetCustomerById(int.Parse(TB_id.Text));
-            TB_FirstName.Text = Customer.FirstName;
-            TB_LastName.Text = Customer.LastName;
-            TB_Mail.Text = Customer.Email;
-            TB_PhoneNr.Text = Customer.Phonenumber;
+            // Valider ID
+            if (!int.TryParse(TB_id.Text, out int id) || id <= 0)
+            {
+
+                MessageBox.Show("Indtast et gyldig ID.", "Fejl", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Hent og vis kundeoplysninger
+            var Customer = _customerService.GetCustomerById(id);
+            if (Customer != null)
+            {
+                TB_FirstName.Text = Customer.FirstName;
+                TB_LastName.Text = Customer.LastName;
+                TB_Mail.Text = Customer.Email;
+                TB_PhoneNr.Text = Customer.Phonenumber;
+            }
+            else
+            {
+                MessageBox.Show("Ingen kunde fundet med det angivne ID.", "Fejl", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
+
+
 
         private void BTN_ShowAllCustomer_Click(object sender, EventArgs e)
         {
@@ -91,13 +191,30 @@ namespace AllInOne
 
         private void BTN_UpdateCustome_Click(object sender, EventArgs e)
         {
-            _customerService.UpdateCustomer(int.Parse(TB_id.Text), TB_FirstName.Text, TB_LastName.Text, TB_Mail.Text, TB_PhoneNr.Text);
+            // Valider input
+            if (string.IsNullOrEmpty(TB_FirstName.Text) || string.IsNullOrEmpty(TB_LastName.Text) || string.IsNullOrEmpty(TB_PhoneNr.Text) || string.IsNullOrEmpty(TB_Mail.Text))
+            {
+                MessageBox.Show("Alle felter skal udfyldes.", "Fejl", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            // Valider telefonnummer format
+            if (!IsValidPhoneNumber(TB_PhoneNr.Text))
+            {
+               MessageBox.Show("Ugyldigt telefonnummer format.", "Fejl", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // Valider e-mail format
+            if (!IsValidEmail(TB_Mail.Text))
+            {
+                MessageBox.Show("Ugyldigt e-mail format.", "Fejl", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+                _customerService.UpdateCustomer(int.Parse(TB_id.Text), TB_FirstName.Text, TB_LastName.Text, TB_Mail.Text, TB_PhoneNr.Text);
             Form1_Load(sender, e);
         }
 
         private void BTN_DeletedCustome_Click(object sender, EventArgs e)
         {
-            DialogResult answer = MessageBox.Show("Vil du slette Kunde?" , "Advarsel", MessageBoxButtons.YesNo);
+            DialogResult answer = MessageBox.Show("Vil du slette Kunde?", "Advarsel", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
             if (answer == DialogResult.Yes)
             {
                 _customerService.DeleteCustomer(int.Parse(TB_id.Text));
@@ -105,7 +222,7 @@ namespace AllInOne
             }
             else
             {
-                
+
             }
         }
     }
--- BLL/Services/BaseCustomerService.cs
+++ BLL/Services/BaseCustomerService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BLL.Services
@@ -11,18 +12,120 @@ namespace BLL.Services
     public abstract class BaseCustomerService : ICustomerService
     {
         private readonly ABS.Interfaces.RePo.ICustomerRePo _customerRePo;
+        /// <summary>
+        /// helpMetode to validate phone number format
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <returns></returns>
+        private bool IsValidPhoneNumber(string phoneNumber)
+        {
+            // kontrollere om telefonnummeret følger det ønskede mønster
+            string pattern = @"^\d{1,8}$"; // Eksempel: 10203040
+
+            return Regex.IsMatch(phoneNumber, pattern); //A Regular Expression (or Regex)
+
+            
+        }
+
+        /// <summary>
+        /// helpMetode to validate the e-mail format
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        private bool IsValidEmail(string email)
+        {
+            // kontroller om e-mailadressen følger det ønskede mønster
+            string pattern = @"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            //var resultat = pattern.Contains("@");
 
 
+            if (pattern.Contains('@'))
+            {
+                return Regex.IsMatch(email, pattern);
+            }
+            else
+            {
+                throw new ArgumentException("Ugyldigt e-mail - mangler et @.");
+            }
+        }
+
+               
+        /// <summary>
+        /// constructor for class
+        /// </summary>
+        /// <param name="customerRePo"></param>
         public BaseCustomerService(ABS.Interfaces.RePo.ICustomerRePo customerRePo)
         {
             _customerRePo = customerRePo;
         }
 
-
+        /// <summary>
+        /// Create a customer and  ensure that the inputs are validated
+        /// </summary>
+        /// <param name="FirstName"></param>
+        /// <param name="LastName"></param>
+        /// <param name="PhoneNr"></param>
+        /// <param name="Mail"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         public bool AddCustomer(string FirstName, string LastName, string PhoneNr, string Mail)
         {
+            // Valider input
+            if (string.IsNullOrEmpty(FirstName) || string.IsNullOrEmpty(LastName) || string.IsNullOrEmpty(PhoneNr) || string.IsNullOrEmpty(Mail))
+            {
+                throw new ArgumentException("Alle felter skal udfyldes.");
+            }
+
+            // Valider telefonnummer format
+            if (!IsValidPhoneNumber(PhoneNr))
+            {
+                throw new ArgumentException("Ugyldigt telefonnummer format.");
+            }
+
+            // Valider e-mail format
+            if (!IsValidEmail(Mail))
+            {
+                throw new ArgumentException("Ugyldigt e-mail format.");
+            }
+
             return _customerRePo.CreateCustomer(FirstName, LastName, PhoneNr, Mail);
+
+            //Laver metoden om til en string ........
+
+            //// Valider input
+            //if (string.IsNullOrEmpty(FirstName) || string.IsNullOrEmpty(LastName) || string.IsNullOrEmpty(PhoneNr) || string.IsNullOrEmpty(Mail))
+            //{
+            //    return "Alle felter skal udfyldes.";
+            //}
+
+            //// Valider telefonnummer format
+            //if (!IsValidPhoneNumber(PhoneNr))
+            //{
+            //    return "Ugyldigt telefonnummer format.";
+            //}
+
+            //// Valider e-mail format
+            //if (!IsValidEmail(Mail))
+            //{
+            //    return "Ugyldigt e-mail format.";
+            //}
+
+            //// Forsøg at oprette kunden
+            //bool success = _customerRePo.CreateCustomer(FirstName, LastName, PhoneNr, Mail);
+
+            //// Tjek om oprettelsen var vellykket
+            //if (success)
+            //{
+            //    return "Kunden blev oprettet succesfuldt.";
+            //}
+            //else
+            //{
+            //    return "Der opstod en fejl under oprettelsen af kunden.";
+            //}
         }
+
+
 
         public bool DeleteCustomer(int id)
         {
@@ -34,18 +137,55 @@ namespace BLL.Services
             return _customerRePo.GetAllCustomers();
         }
 
+        /// <summary>
+        /// Find a customer by ID og validate the int input
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         public ICustomerModel GetCustomerById(int id)
-        { 
+        {
+            // Valider input
+
+
+            if (id <= 0)
+            {
+                throw new ArgumentException("ID skal være større end nul.");
+            }
+
             return _customerRePo.GetCustomerById(id);
         }
-
-
-
+        /// <summary>
+        /// Update a customer and validate the new inputs
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="FirstName"></param>
+        /// <param name="LastName"></param>
+        /// <param name="PhoneNr"></param>
+        /// <param name="Mail"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         public bool UpdateCustomer(int Id, string FirstName, string LastName, string PhoneNr, string Mail)
         {
+            // Valider input
+            if (string.IsNullOrEmpty(FirstName) || string.IsNullOrEmpty(LastName) || string.IsNullOrEmpty(PhoneNr) || string.IsNullOrEmpty(Mail))
+            {
+                throw new ArgumentException("Alle felter skal udfyldes.");
+            }
+            // Valider telefonnummer format
+            if (!IsValidPhoneNumber(PhoneNr))
+            {
+                throw new ArgumentException("Ugyldigt telefonnummer format.");
+            }
+
+            // Valider e-mail format
+            if (!IsValidEmail(Mail))
+            {
+                throw new ArgumentException("Ugyldigt e-mail format.");
+            }
+
+
             return _customerRePo.UpdateCustomer(Id, FirstName, LastName, PhoneNr, Mail);
         }
-
     }
-
 }
