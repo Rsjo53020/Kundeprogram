b643f248ccb6b7a0e97d4c8c5633ea1f09149f95
CHRISTOFFER\chris <chsk52994@edu.ucl.dk>
2023-10-16 18:10:31 +0200

    mere async

--- ABS/Interfaces/Services/ICustomerService.cs
+++ ABS/Interfaces/Services/ICustomerService.cs
@@ -9,15 +9,15 @@ namespace ABS.Interfaces.Services
 {
     public interface ICustomerService
     {
-        public List<Models.ICustomerModel> GetAllCustomersAsync();
+        public Task<List<Models.ICustomerModel>> GetAllCustomersAsync();
 
-        public Models.ICustomerModel GetCustomerByIdAsync(int id);
+        public Task<Models.ICustomerModel> GetCustomerByIdAsync(int id);
 
-        public bool AddCustomerAsync(string FirstName, string LastName, string PhoneNr, string Mail);
+        public Task<bool> AddCustomerAsync(string FirstName, string LastName, string PhoneNr, string Mail);
         
 
-        public bool UpdateCustomerAsync(int Id, string FirstName, string LastName, string PhoneNr, string Mail);
+        public Task<bool> UpdateCustomerAsync(int Id, string FirstName, string LastName, string PhoneNr, string Mail);
 
-        public bool DeleteCustomerAsync(int id);
+        public Task<bool> DeleteCustomerAsync(int id);
     }
 }
